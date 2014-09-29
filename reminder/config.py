95d208f262b288eb4fa0dd4cc119ec1a51c92ac8
phouse512 <philiphouse2015@u.northwestern.edu>
2014-09-29 00:20:45 -0500

    test

--- reminder/config.py
+++ reminder/config.py
@@ -2,7 +2,7 @@ import os
 PWD = os.path.abspath(os.curdir)
 
 DEBUG=True
-SQLALCHEMY_DATABASE_URI = 'postgres://PhilipHouse:house@localhost/reminder'
+SQLALCHEMY_DATABASE_URI = 'postgres://qiyyulwagfkvzt:j_k8ydtSIEny7mwA87mCj68d9o@ec2-54-83-204-104.compute-1.amazonaws.com:5432/d9dppr2muk5dct'
 SECRET_KEY = 'thisissecret'
 CSRF_ENABLED = True
 SESSION_PROTECTION = 'strong'
